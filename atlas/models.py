3f75f91141f863a44141afae45ef7c99b20d8fb9
TylerRudie <rudiet@gmail.com>
2015-09-17 09:11:01 -0500

    starting oms

--- atlas/models.py
+++ atlas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class event(models.Model):
+    evid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+
+class hardware(models.Model):
+    hwid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
