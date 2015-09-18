3f5d8cf2cfd4f289a3f61446c7256cb0c354ace6
TylerRudie <rudiet@gmail.com>
2015-09-18 17:44:40 -0500

    Base Config of Atlas App

--- atlas/admin.py
+++ atlas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import event, hardware
+
+admin.site.register(event)
+
+admin.site.register(hardware)
+
 # Register your models here.
--- atlas/models.py
+++ atlas/models.py
@@ -2,10 +2,39 @@ from django.db import models
 import uuid
 # Create your models here.
 
-class event(models.Model):
-    evid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-
 
 class hardware(models.Model):
-    hwid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status_choices = (
+        ('a','Active'),
+        ('s','Standby'),
+        ('i','Inactive'),
+    )
+    hwId      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    serialNum = models.CharField(max_length=100)
+    desc      = models.CharField(max_length=100)
+    config    = models.CharField(max_length=100)
+    status    = models.CharField(max_length=100, choices = status_choices, default='Active' )
+
+    def __str__(self):
+        return self.serialNum
+
+class event(models.Model):
+    status_choices = (
+        ('e','Event'),
+        ('i','Inactive'),
+        ('p','Pool'),
+    )
+
+    evId       = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name       = models.CharField(max_length=100)
+    status     = models.CharField(max_length=100, choices=status_choices, default='Event')
+    startDate  = models.DateField(max_length=100)
+    endDate    = models.DateField(max_length=100)
+    hwAssigned = models.ManyToManyField(hardware)
+
+    def __str__(self):
+        return self.name
+
+
+
+
