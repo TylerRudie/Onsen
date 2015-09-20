19ebc5c5725d6ae8e80dd3a0ad389ca806d03755
TylerRudie <rudiet@gmail.com>
2015-09-20 18:13:29 -0500

    Created Contact and Contact_Event objects and set up editing in event admin page.

--- atlas/admin.py
+++ atlas/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 
-from .models import event, hardware
+from .models import event, hardware, contact, contact_event
 
-admin.site.register(event)
 
-admin.site.register(hardware)
+class EventInline(admin.StackedInline):
+    model = event.hwAssigned.through
+    extra = 1
 
+
+class ctEventID(admin.TabularInline):
+    model = event.ctAssigned.through
+    extra = 1
+    verbose_name = 'Assigned Contact'
+
+class HardwareAdmin(admin.ModelAdmin):
+    fields = ['serialNum','desc','config','status']
+    inlines = [EventInline]
+
+
+class EventAdmin(admin.ModelAdmin):
+    fields = ['name','status','startDate','endDate','hwAssigned']
+    inlines = [ctEventID]
+
+
+admin.site.register(event, EventAdmin)
+admin.site.register(contact )
+admin.site.register(hardware, HardwareAdmin)
+admin.site.register(contact_event)
 # Register your models here.
--- atlas/models.py
+++ atlas/models.py
@@ -11,12 +11,34 @@ class hardware(models.Model):
     )
     hwId      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     serialNum = models.CharField(max_length=100)
-    desc      = models.CharField(max_length=100)
-    config    = models.CharField(max_length=100)
+    desc      = models.CharField(max_length=100, blank=True)
+    config    = models.CharField(max_length=100, blank=True)
     status    = models.CharField(max_length=100, choices = status_choices, default='Active' )
+
+
 
     def __str__(self):
         return self.serialNum
+
+
+class contact(models.Model):
+    ctId      = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    firstName = models.CharField(max_length=100, blank=True)
+    lastName  = models.CharField(max_length=100, blank=True)
+    address1  = models.CharField(max_length=100, blank=True)
+    address2  = models.CharField(max_length=100, blank=True)
+    city      = models.CharField(max_length=100, blank=True)
+    state     = models.CharField(max_length=100, blank=True)
+    zip       = models.CharField(max_length=100, blank=True)
+    phone     = models.CharField(max_length=100, blank=True)
+    email     = models.EmailField(blank=True)
+    company   = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName + ' <' + self.email + '>'
+
+
+
+
 
 class event(models.Model):
     status_choices = (
@@ -28,13 +50,22 @@ class event(models.Model):
     evId       = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name       = models.CharField(max_length=100)
     status     = models.CharField(max_length=100, choices=status_choices, default='Event')
-    startDate  = models.DateField(max_length=100)
-    endDate    = models.DateField(max_length=100)
-    hwAssigned = models.ManyToManyField(hardware)
+    startDate  = models.DateField(max_length=100, blank=True)
+    endDate    = models.DateField(max_length=100, blank=True)
+    hwAssigned = models.ManyToManyField(hardware, blank=True)
+    ctAssigned = models.ManyToManyField(contact, through='contact_event', blank=True)
 
     def __str__(self):
         return self.name
 
 
+class contact_event(models.Model):
 
+    ctId       = models.ForeignKey(contact)
+    evId       = models.ForeignKey(event)
+    isShipping = models.BooleanField(default=False)
+    isInst     = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.ctId.firstName + ' ' + self.ctId.lastName + '<>' + self.evId.name
 
